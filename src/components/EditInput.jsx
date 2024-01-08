25c645d8a858e79839b9a5112526e534c53285c8
thantmyo <thantmyo@cut.cz>
2024-01-08 20:28:52 +0100

    mtt2

--- src/components/EditInput.jsx
+++ src/components/EditInput.jsx
@@ -2,30 +2,25 @@ import PropTypes from "prop-types";
 import React, { useState } from "react";
 
 export const EditInput = ({
-  editMode,
-  name,
-  setName,
-  handleUpdate,
+  edit,
+  data,
+  setData,
   placeholder,
   isNumber = false,
 }) => {
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleUpdate();
-      }}
-    >
-      <input
-        className="w-full disabled:bg-transparent -z-10"
-        placeholder={placeholder}
-        type={isNumber ? "number" : "text"}
-        disabled={!editMode}
-        value={name}
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      />
-    </form>
+    <div className="w-40 overflow-hidden">
+      {edit ? (
+        <input
+          className="bg-black/20"
+          placeholder={placeholder}
+          autoFocus
+          value={data ?? ""}
+          onChange={(e) => setData(e.target.value)}
+        ></input>
+      ) : (
+        data
+      )}
+    </div>
   );
 };
--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -1,124 +1,183 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Link, generatePath } from "react-router-dom";
+import { getAllEntry, updateExistingEntry } from "../service";
+import { EditInput } from "../components/EditInput";
 
 export const BooksPage = () => {
-  const [openTab, setOpenTab] = useState(1);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    getAllEntry("/book").then((res) => {
+      console.log(res);
+      setData(res);
+    });
+  }, []);
 
   return (
     <div className="overflow-x-auto">
-      <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-        <thead className="text-center">
-          <tr>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              id
-            </th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              Title
-            </th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              ISBN
-            </th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              rating
-            </th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              genre
-            </th>
-            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-              publishYear
-            </th>
+      {data && (
+        <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+          <thead className="text-left">
+            <tr>
+              <th className="px-4 py-2 font-medium text-gray-900">id</th>
+              <th className="px-4 py-2 font-medium text-gray-900">Title</th>
+              <th className="px-4 py-2 font-medium text-gray-900">ISBN</th>
+              <th className="px-4 py-2 font-medium text-gray-900">rating</th>
+              <th className="px-4 py-2 font-medium text-gray-900">genre</th>
+              <th className="px-4 py-2 font-medium text-gray-900">
+                publishYear
+              </th>
 
-            <th className="px-4 py-2"></th>
-          </tr>
-        </thead>
+              <th className="px-4 py-2"></th>
+            </tr>
+          </thead>
 
-        <tbody className="divide-y divide-gray-200">
-          <BookRow
-            id={1}
-            title={"HP"}
-            isbn={1}
-            rating={1}
-            genre={1}
-            pubYear={2022}
-          />
-          <BookRow
-            id={1}
-            title={"HP"}
-            isbn={1}
-            rating={1}
-            genre={1}
-            pubYear={2022}
-          />
-          <BookRow
-            id={1}
-            title={"HP"}
-            isbn={1}
-            rating={1}
-            genre={1}
-            pubYear={2022}
-          />
-          <BookRow
-            id={1}
-            title={"HP"}
-            isbn={1}
-            rating={1}
-            genre={1}
-            pubYear={2022}
-          />
-        </tbody>
-      </table>
+          <tbody className="divide-y divide-gray-200">
+            {data.map((book) => (
+              <BookRow
+                key={book.bookId}
+                bookId={book.bookId}
+                bookTitle={book.bookTitle}
+                bookIsbn={book.isbn}
+                bookGenre={book.genre}
+                bookPubYear={book.publishYear}
+                bookRating={book.rating}
+                bookFavourites={book.favourites}
+                bookReadBy={book.readBy}
+                bookWroteBy={book.wroteBy}
+              ></BookRow>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
 
-export const BookRow = ({ id, title, isbn, rating, genre, pubYear }) => {
+export const BookRow = ({
+  bookId,
+  bookTitle,
+  bookIsbn,
+  bookRating,
+  bookGenre,
+  bookPubYear,
+  bookFavourites,
+  bookReadBy,
+  bookWroteBy,
+}) => {
+  const [title, setTitle] = useState(bookTitle);
+  const [isbn, setIsbn] = useState(bookIsbn);
+  const [rating, setRating] = useState(bookRating);
+  const [genre, setGenre] = useState(bookGenre);
+  const [pubYear, setPubYear] = useState(bookPubYear);
+
+  const [edit, setEdit] = useState();
+
+  function handleEdit() {
+    setEdit(!edit);
+    if (title == "") setTitle(bookTitle);
+    if (isbn == "") setIsbn(bookIsbn);
+    if (rating == "") setRating(bookRating);
+    if (genre == "") setGenre(bookGenre);
+    if (pubYear == "") setPubYear(bookPubYear);
+  }
+
+  const handleUpdate = () => {
+    updateExistingEntry("/book/", bookId, {
+      bookTitle: title,
+      isbn: isbn,
+      genre: genre,
+      publishYear: pubYear,
+      rating: rating,
+      favourites: bookFavourites,
+      readBy: bookReadBy,
+      wroteBy: bookWroteBy,
+    });
+
+    handleEdit();
+  };
+
   return (
     <tr>
-      <td className="whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900">
-        {id}
+      <td className="text-left px-4 py-2 font-medium text-gray-900">
+        {bookId}
       </td>
-      <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-        {title}
+      <td className="text-left py-2 text-gray-700">
+        <EditInput
+          edit={edit}
+          data={title}
+          setData={setTitle}
+          placeholder={"Enter Title"}
+        ></EditInput>
       </td>
-      <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-        {isbn}
+      <td className="text-left px-4 py-2 text-gray-700">
+        <EditInput
+          edit={edit}
+          data={isbn}
+          setData={setIsbn}
+          placeholder={"Enter ISBN"}
+        ></EditInput>
       </td>
-      <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-        {rating}
+      <td className="text-left px-4 py-2 text-gray-700">
+        <EditInput
+          edit={edit}
+          data={rating}
+          setData={setRating}
+          placeholder={"Enter Rating"}
+        ></EditInput>
       </td>
-      <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-        {genre}
+      <td className="text-left px-4 py-2 text-gray-700">
+        <EditInput
+          edit={edit}
+          data={genre}
+          setData={setGenre}
+          placeholder={"Enter Genre"}
+        ></EditInput>
       </td>
-      <td className="whitespace-nowrap text-center px-4 py-2 text-gray-700">
-        {pubYear}
+      <td className="text-left px-4 py-2 text-gray-700">
+        <EditInput
+          edit={edit}
+          data={pubYear}
+          setData={setPubYear}
+          placeholder={"Enter Year"}
+        ></EditInput>
       </td>
 
       {/* View */}
-      <td className="whitespace-nowrap text-center px-4 py-2">
-        <a
-          href="#"
-          className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-        >
+      <td className="text-left px-4 py-2">
+        <Link className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700">
           View
-        </a>
+        </Link>
       </td>
       {/* Edit */}
-      <td className="whitespace-nowrap text-center px-4 py-2">
-        <a
-          href="#"
-          className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-        >
-          View
-        </a>
+      <td className="text-left px-4 py-2">
+        {!edit && (
+          <button
+            onClick={() => {
+              console.log("edit");
+              setEdit(!edit);
+            }}
+            className="inline-block rounded bg-black/70 px-4 py-2 text-xs font-medium text-white hover:bg-black/80"
+          >
+            Edit
+          </button>
+        )}
+
+        {edit && (
+          <button
+            onClick={() => handleUpdate()}
+            className="inline-block rounded bg-green-600 px-4 py-2 text-xs font-medium text-white hover:bg-green-800"
+          >
+            Save
+          </button>
+        )}
       </td>
 
       {/* Delete */}
-      <td className="whitespace-nowrap text-center px-4 py-2">
-        <a
-          href="#"
-          className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-        >
-          View
-        </a>
+      <td className="text-left px-4 py-2">
+        <Link className="inline-block rounded bg-red-600 px-4 py-2 text-xs font-medium text-white hover:bg-red-700">
+          Delete
+        </Link>
       </td>
     </tr>
   );
